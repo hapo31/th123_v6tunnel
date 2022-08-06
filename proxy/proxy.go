eab90046dc9f6e7ca726702598db6f93abddb379
happou31 <happo31@gmail.com>
2022-08-06 14:32:23 +0900

    fix: 出力 port 番号

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -92,7 +92,7 @@ func (p *Proxy) StartClient(sendAddrStr string) (chan bool, error) {
 
 func (p *Proxy) StartServer(proxyPort int) (chan bool, error) {
 	remoteAddr, err := parseIP(fmt.Sprintf("[::1]:%d", proxyPort+1))
-	fmt.Printf("Receive from %d\n", proxyPort)
+	fmt.Printf("Receive from %d\n", remoteAddr.Port)
 	if err != nil {
 		return nil, err
 	}
